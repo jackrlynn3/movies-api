2cd9cfe90307b87f4c3978ca944e7d530c9ec2fe
jackrlynn3 <jackrlynn@gmail.com>
2022-07-07 15:05:47 -0500

    Added comparison operator to Media class

--- class/Media.py
+++ class/Media.py
@@ -36,3 +36,13 @@ class Media:
     # Add character method
     def add_character(self, character):
         self.characters.append(character)
+
+    # Overwrite magic methods
+    def __gt__(self, other):
+        return self.episode_id > other.episode_id
+    
+    def __ge__(self,other):
+        return self.episode_id >= other.episode_id
+    
+    def __eq__(self,other):
+        return self.episode_id == other.episode_id
